de6e5f7cf766e382f2d12b5251d445b947bbef8f
Simanraj Sadana <simanrajsadana@gmail.com>
2024-10-25 18:32:22 +0200

    Dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,104 @@
+return {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup {
+            theme = "doom",
+            config = {
+                header = {
+                    "                                                                                                                        ",
+                    "                                                                                                                        ",
+                    "                                                                                                                        ",
+                    "                                                                            *#######:               ",
+                    "                                                                        ###====++***##:             ",
+                    "                                                                    -##=---===++****###:            ",
+                    "                                                                 -##=::---====++***####+            ",
+                    "                    :=####*****#####-:                        :##=:::-----===++***#####+            ",
+                    "                  *##*+====++++++***#####:                 .##=----------===++***######=            ",
+                    "                =##*+=========+++****########.          .##=-====------==-===+***######:            ",
+                    "               %@#++===========++*****##########+     *#=-=======--:::::::-======*####*             ",
+                    "              =@@#*============+++*****##############====::                    -===*##-             ",
+                    "             .@@@@@#*===========++*****#####%@@@@%=*###*:                      -@@==*#              ",
+                    "             @@@@@@@@@@@@%%######%%@@@@@@@@@@@#=*###########=:             :+@@@@@@===              ",
+                    "            *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@==#############%%%@@@@@@@%@@@@@@@@@@@@@@==:              ",
+                    "           =@@@@@@@@@@@@@@@@@@@@@@@@@@@+=*%%%%#########%%%%%@@@@@@@@@@@@@@@@@@@@@@@=:               ",
+                    "          :@@@@@@@@@@@@@@@@@@@@@@@%==:===*%%%%%%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@+-                ",
+                    "          *@@@@@@@@@@@@@@@@@@#===:        ===+%@%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@=:                 ",
+                    "          =@@@@@@@@@@@@#====                   ===#@@@@@@@@@@@@@@@@@@@@@@@@@@@@==                   ",
+                    "            -========:                              ==+%@@@@@@@@@@@@@@@@@@@@+=                      ",
+                    "                                                         .===*%@@@@@@%*==:                          ",
+                    "",
+                    "",
+                    "",
+                },
+                center = {
+                    {
+                        icon = ' ',
+                        icon_hl = 'group',
+                        desc = 'Find file',
+                        desc_hl = 'group',
+                        key = 'f',
+                        key_hl = 'group',
+                        key_format = ' [f]', -- `%s` will be substituted with value of `key`
+                        action = ':Telescope find_files',
+                    },
+                    {
+                        icon = ' ',
+                        icon_hl = 'group',
+                        desc = 'Grep Search',
+                        desc_hl = 'group',
+                        key = 'g',
+                        key_hl = 'group',
+                        key_format = ' [g]', -- `%s` will be substituted with value of `key`
+                        action = ':Telescope live_grep',
+                    },
+                    {
+                        icon = ' ',
+                        icon_hl = 'group',
+                        desc = 'Open Config',
+                        desc_hl = 'group',
+                        key = 'c',
+                        key_hl = 'group',
+                        key_format = ' [c]', -- `%s` will be substituted with value of `key`
+                        action = ':Telescope find_files search_dirs={"~/Programming/config/nvim/"}',
+                    },
+                    {
+                        icon = '󰒲 ',
+                        icon_hl = 'group',
+                        desc = 'Lazy',
+                        desc_hl = 'group',
+                        key = 'l',
+                        key_hl = 'group',
+                        key_format = ' [l]', -- `%s` will be substituted with value of `key`
+                        action = ':Lazy',
+                    },
+                    {
+                        icon = ' ',
+                        icon_hl = 'group',
+                        desc = 'Mason',
+                        desc_hl = 'group',
+                        key = 'm',
+                        key_hl = 'group',
+                        key_format = ' [m]', -- `%s` will be substituted with value of `key`
+                        action = ':Mason',
+                    },
+                    {
+                        icon = '󰈆 ',
+                        icon_hl = 'group',
+                        desc = 'Quit',
+                        desc_hl = 'group',
+                        key = 'q',
+                        key_hl = 'group',
+                        key_format = ' [q]', -- `%s` will be substituted with value of `key`
+                        action = ':confirm q',
+                    },
+                },
+                footer = {
+                    "Neovim loaded " .. require('lazy').stats().count .. " plugins",
+                    "Have a great coding session!",
+                }
+            }
+        }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+}
