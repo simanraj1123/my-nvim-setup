8689df67fb2707a87305a786eb76e2701effd003
Simanraj Sadana <simanrajsadana@gmail.com>
2024-11-21 13:38:20 +0100

    Changed default theme

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("config.lazy")
 require("config.keymaps")
 require("config.autocommands")
 
-vim.cmd("colorscheme catppuccin-mocha")
+-- vim.cmd("colorscheme catppuccin-mocha")
 -- vim.cmd("colorscheme plain")
 -- vim.cmd("colorscheme github_light_default")
+vim.cmd("colorscheme github_dark_default")
