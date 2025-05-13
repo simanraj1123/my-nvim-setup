648a4d89a49fadec0356f49aca0e382cc9f5c186
Simanraj Sadana <simanrajsadana@gmail.com>
2025-05-14 01:42:37 +0200

    google style python docstrings

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require("config.keymaps")
 require("config.autocommands")
 
 vim.cmd("colorscheme catppuccin-mocha")
+vim.g.doge_doc_standard_python = 'google'
 -- vim.cmd("colorscheme plain")
 -- vim.cmd("colorscheme github_light_default")
 -- vim.cmd("colorscheme github_dark_default")
